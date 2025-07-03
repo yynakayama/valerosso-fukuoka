45053de79f17a49aae6f184d2d8bf62059f27974
yynakayama <y_nakayama@vantec2000.com>
2025-07-03 11:02:49 +0900

    SEO対策

---
+++ public/js/analytics.js
@@ -0,0 +1,8 @@
+// Google Analytics 4 (GA4) Configuration
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-MDNJC53C4C', {
+  page_title: document.title,
+  page_location: window.location.href
+}); 
